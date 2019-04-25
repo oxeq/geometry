ced03e35bc0b0dab5a85f3d8ec24982b4ae257a1
oxeq <xotaby4@yandex.ru>
2019-04-26 04:11:46 +0700

    Implement data entry and output

---
+++ check_correct.c
@@ -0,0 +1,62 @@
+#include "check_correct.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define l 300
+
+int check_correct(char* figure, Circle* c)
+{
+	int i = 7;
+	int j = 0;
+	int k = 0;
+char circle[7] = {'c', 'i', 'r', 'c', 'l', 'e', '\0'};
+
+for (k = 0; circle[k] != '\0'; k++){
+	if(figure[k] != circle[k]){
+		printf("Name Error\n");
+		exit(1);
+		}
+	}
+
+	for (j=0; figure[i] != ' '; i++, j++) {
+		if ((figure[i] < '0' || figure[i] > '9' || figure[i] == ' ')
+            && figure[i] != '.') {
+			printf("Incorrect coordinates\n");
+			exit(1);
+		}
+
+	circle_str_x[j] = figure[i];
+    }
+
+    c->x = atof(circle_str_x);
+    i++;
+
+    for (j = 0; figure[i] != ','; i++, j++) {
+        if ((figure[i] < '0' || figure[i] > '9' || figure[i] == ' ')
+            && figure[i] != '.') {
+            printf("Incorrect coordinates\n");
+            exit(1);
+        }
+        circle_str_y[j] = figure[i];
+    }
+    i += 2;
+    c->y = atof(circle_str_y);
+
+    for (j = 0; figure[i] != ')'; i++, j++) {
+        if ((figure[i] < '0' || figure[i] > '9' || figure[i] == ' ')
+            && figure[i] != '.') {
+            printf("Incorrect coordinates\n");
+            exit(1);
+        }
+        circle_str_rad[j] = figure[i];
+    }
+    c->rad = atof(circle_str_rad);
+
+    if (c->rad == 0) {
+        printf("Incorrect radius set\n");
+        return 1;
+    }
+
+    return 0;
+}
+	
---
+++ check_correct.h
@@ -0,0 +1,17 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#define L 300
+
+typedef struct circle {
+	double x;
+	double y;
+	double rad;
+} Circle;
+
+int check_correct();
+char circle_str_x[L];
+char circle_str_y[L];
+char circle_str_rad[L];
+char figure[L];
+
+#endif
